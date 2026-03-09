bdaad2b1b88c0a38671a68ee8881ad881028ebad
Jayanth H A <jayanthjayy12@gmail.com>
2026-03-09 18:06:23 +0000

    Initial Vexora project setup with CMake, vcpkg, spdlog and GoogleTest

---
+++ build/CMakeFiles/vexora_tests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vexora_tests.dir/tests/sample_test.cpp.o"
+  "CMakeFiles/vexora_tests.dir/tests/sample_test.cpp.o.d"
+  "vexora_tests"
+  "vexora_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vexora_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/jayanth/projects/Vexora
+# Build directory: /home/jayanth/projects/Vexora/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[vexora_tests]=] "/home/jayanth/projects/Vexora/build/vexora_tests")
+set_tests_properties([=[vexora_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/jayanth/projects/Vexora/CMakeLists.txt;33;add_test;/home/jayanth/projects/Vexora/CMakeLists.txt;0;")
